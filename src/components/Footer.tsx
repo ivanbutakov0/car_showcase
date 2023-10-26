e707f6bec1acaaf2a8dfaab8a426af660f13b0ad
Ivan Butakov <ivanbutakov000@gmail.com>
2023-10-26 21:06:20 +0500

    Create footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,47 @@
+import { footerLinks } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
+
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<footer>
+			<div className='flex pt-10 px-6 md:px-16 pb-20 w-full justify-between flex-col md:flex-row border border-gray-200 gap-10'>
+				<div className='text-gray-700'>
+					<Link href='/'>
+						<Image src='/logo.svg' alt='logo' width={118} height={18} />
+					</Link>
+					<p className='mt-6'>Carhub 2023</p>
+					<p>All Rights Reserved &copy;</p>
+				</div>
+				<div className='flex gap-10 md:gap-20 width-full flex-1 flex-wrap justify-start md:justify-end'>
+					{footerLinks.map(link => (
+						<div
+							key={link.title}
+							className='flex flex-col gap-1 md:gap-4 min-w-[170px]'
+						>
+							<h3 className='font-bold'>{link.title}</h3>
+							{link.links.map(item => (
+								<Link
+									key={item.title}
+									href={item.url}
+									className='text-gray-500'
+								>
+									{item.title}
+								</Link>
+							))}
+						</div>
+					))}
+				</div>
+			</div>
+
+			<div className='py-10 px-6 md:px-16 gap-5 flex flex-col md:flex-row justify-between'>
+				<p className='text-gray-700'>@2023 CarHub. All rights reserved</p>
+				<div className='text-gray-500 flex gap-10'>
+					<Link href='/'>Privacy & Policy</Link>
+					<Link href='/'>Terms & Condition</Link>
+				</div>
+			</div>
+		</footer>
+	)
 }
 export default Footer
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { CustomButton } from '.'
 const Navbar = () => {
 	return (
 		<header className='absolute w-full z-10  '>
-			<nav className='max-w-[1440px] mx-auto flex justify-between items-center py-4 px-16 '>
+			<nav className='max-w-[1440px] mx-auto flex justify-between items-center py-4 p-6 md:px-16 '>
 				<Link href='/' className='p-1'>
 					<Image src='/logo.svg' alt='logo' width={118} height={18} />
 				</Link>
